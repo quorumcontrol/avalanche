c32e7dbe53a29e2fd6e8510ab4c8e28725edd139
Topper Bowers <topper@toppingdesign.com>
2018-06-16 19:30:46 +0200

    initial snowball implementation

--- snowball/snowball.go
+++ snowball/snowball.go
@@ -1,60 +1,137 @@
 package snowball
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+	"fmt"
+	"github.com/google/uuid"
+)
 
 type NodeId string
 
 type NodeSystem struct {
 	N int
 	K int
+	Alpha int // slight deviation to avoid floats, just calculate k*a from the paper
 	Beta int
 	Nodes NodeHolder
+	ArtificialLatency int // in ms
 }
 
-/*
-
-procedure snowballLoop(u, col0 ∈ {R, B, ⊥})
-2: col := col0, lastcol := col0, cnt := 0
-3: d[R] := 0, d[B] := 0
-4: while undecided do
-5: if col = ⊥ then continue
-6: K := sample(N \u, k)
-7: P := [query(v, col) for v ∈ K]
-8: for col0 ∈ {R, B} do
-9: if P.count(col0
-) ≥ α · k then
-10: d[col0
-]++
-11: if d[col0
-] > d[col] then
-12: col := col0
-13: if col0
-6= lastcol then
-14: lastcol := col0
-, cnt := 0
-15: else
-16: if ++cnt > β then accept(col)
-Figure 3: Snowball.
- */
+type stateQuery struct {
+	state string
+	responseChan chan string
+}
 
 type Node struct {
 	Id NodeId
 	State string
 	LastState string
-	Incoming chan string
+	Incoming chan stateQuery
+	StopChan chan bool
 	Counts map[string]int
+	Count int
 	System *NodeSystem
+	Accepted bool
+	didKickOff bool
 }
 
 type NodeHolder map[NodeId]*Node
 
-func (n Node) OnReceive(state string) {
-	sample := make([]*Node, n.System.K)
-	for {
-		for i := 0; i < len(sample); i++ {
-			sample[i] = randNode(n.System.Nodes)
-		}
+func NewNode(system *NodeSystem) *Node {
+	return &Node{
+		Id: NodeId(uuid.New().String()),
+		Incoming: make(chan stateQuery, system.Beta),
+		StopChan: make(chan bool),
+		Counts: make(map[string]int),
+		System: system,
+	}
+}
 
+func (n *Node) Start() error {
+	go func() {
+		for {
+			select {
+			case <-n.StopChan:
+				break
+			case query := <- n.Incoming:
+				n.OnQuery(query.state, query.responseChan)
+			}
+		}
+	}()
+	return nil
+}
+
+func (n *Node) Stop() error {
+	n.StopChan <- true
+	return nil
+}
+
+func (n *Node) OnQuery(state string, responseChan chan string) {
+	//fmt.Printf("node %v received onQuery with state: %v\n", n.Id, state)
+	if n.State == "" {
+		n.State = state
+		n.LastState = state
+	}
+	<-time.After(time.Duration(n.System.ArtificialLatency) * time.Millisecond)
+	responseChan <- n.State
+
+	if !n.didKickOff {
+		n.didKickOff = true
+		go func() {
+			for {
+				responses := make(map[string]int)
+				for i := 0; i < n.System.K; i++ {
+					node := randNode(n.System.Nodes)
+					//fmt.Printf("node %v is querying %v\n", n.Id, node.Id)
+					resp,err := node.SendQuery(state)
+					if err == nil {
+						responses[resp]++
+					}
+					//fmt.Printf("node %v received response %v from %v\n", n.Id, resp, node.Id)
+				}
+				//fmt.Printf("node %v responses: %v\n", n.Id, responses)
+				for state,count := range responses {
+					if count > n.System.Alpha {
+						n.Counts[state]++
+						if n.Counts[state] > n.Counts[n.State] {
+							n.State = state
+						}
+						if n.State == n.LastState {
+							//fmt.Printf("node %v inc count from %d\n", n.Id, n.Count)
+							n.Count++
+						} else {
+							//fmt.Printf("node %v reset count because %v != %v\n", n.Id, n.LastState, n.State)
+							n.LastState = state
+							n.Count = 0
+						}
+					} else {
+						//fmt.Printf("node %v did not get to alpha\n", n.Id)
+					}
+				}
+				if n.Count > n.System.Beta {
+					//fmt.Printf("stopping because beta reached\n")
+					break
+				}
+			}
+			n.Accepted = true
+		}()
+	}
+}
+
+func (n *Node) SendQuery(state string) (string,error) {
+	t := time.After(10 * time.Second)
+	respChan := make(chan string)
+	n.Incoming <- stateQuery{
+		state: state,
+		responseChan: respChan,
+	}
+	select {
+	case <-t:
+		fmt.Printf("timeout on sendquery")
+		return "", fmt.Errorf("timeout")
+	case resp := <-respChan:
+		return resp,nil
 	}
 }
 
--- snowball/snowball_test.go
+++ snowball/snowball_test.go
@@ -1,1 +1,61 @@
 package snowball
+
+import (
+	"testing"
+	"time"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+)
+
+func TestNodes(t *testing.T) {
+	system := &NodeSystem{
+		N: 1000,
+		K: 10,
+		Alpha: 8, // slight deviation to avoid floats, just calculate k*a from the paper
+		Beta: 10,
+		ArtificialLatency: 100, // in ms
+	}
+
+	holder := make(NodeHolder)
+	for i := 0; i < system.N; i++ {
+		node :=  NewNode(system)
+		node.Id = NodeId(strconv.Itoa(i)) // for readability of the logs
+		holder[node.Id] = node
+		node.Start()
+	}
+	defer func() {
+		for _,node := range holder {
+			node.Stop()
+		}
+	}()
+
+	system.Nodes = holder
+
+	node := randNode(holder)
+	resp,err := node.SendQuery("test")
+	assert.Nil(t, err)
+	assert.Equal(t, "test", resp)
+
+
+	start := time.Now()
+	for {
+		<- time.After(1 * time.Second)
+		count := 0
+		for _,node := range holder {
+			if node.Accepted {
+				count++
+			}
+		}
+		if count == system.N {
+			break
+		}
+	}
+	stop := time.Now()
+	t.Logf("start: %v, stop: %v: diff: %d", start, stop, stop.Unix()-start.Unix())
+
+	for _,node := range holder {
+		assert.True(t, node.Accepted, "node %v", node.Id)
+		//t.Logf("node: %s, accepted? %v", node.Id, node.Accepted)
+	}
+
+}
